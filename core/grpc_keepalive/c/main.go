f8f1fd0ce0169fa25472e7e9be54e6643b05c725
mangenotwork <2912882908@qq.com>
2022-05-07 15:48:10 +0800

    add grpc keepalive

---
+++ core/grpc_keepalive/c/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"go_script/grpc_2/pb/echo"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/keepalive"
+	"log"
+	"time"
+)
+
+var addr = flag.String("addr", "localhost:50052", "the address to connect to")
+
+var kacp = keepalive.ClientParameters{
+	Time:                10 * time.Second, // send pings every 10 seconds if there is no activity
+	Timeout:             time.Second,      // wait 1 second for ping ack before considering the connection dead
+	PermitWithoutStream: true,             // send pings even without active streams
+}
+
+func main() {
+	flag.Parse()
+
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithKeepaliveParams(kacp))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	c := echo.NewEchoClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Minute)
+	defer cancel()
+	fmt.Println("Performing unary request")
+	res, err := c.UnaryEcho(ctx, &echo.EchoRequest{Message: "keepalive demo"})
+	if err != nil {
+		log.Fatalf("unexpected error from UnaryEcho: %v", err)
+	}
+	fmt.Println("RPC response:", res)
+	//select {} // Block forever; run with GODEBUG=http2debug=2 to observe ping frames and GOAWAYs due to idleness.
+}
---
+++ core/grpc_keepalive/pb/go.mod
@@ -0,0 +1,8 @@
+module go_script/grpc_2/pb
+
+go 1.16
+
+require (
+	google.golang.org/grpc v1.46.0
+	google.golang.org/protobuf v1.28.0
+)
