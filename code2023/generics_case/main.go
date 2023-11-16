4be40e76d1fffc07303055d92e63809ce5f024a2
root <liman@nggame888.com>
2023-11-16 11:30:29 +0800

    .

---
+++ code2023/generics_case/main.go
@@ -0,0 +1,112 @@
+/*
+	泛型
+*/
+
+package main
+
+import (
+	"log"
+)
+
+func main() {
+
+	test1 := []int{1, 1, 2, 2, 3, 3}
+	test2 := []string{"1", "1", "2", "2", "3", "3"}
+
+	log.Println(SliceRemoveDuplicate(test1))
+	log.Println(SliceRemoveDuplicate(test2))
+
+}
+
+type SliceType interface {
+	int | uint | int8 | uint8 | int16 | uint16 | int32 | uint32 | int64 | uint64 | float32 | float64 | string | bool |
+		*int | *uint | *int8 | *uint8 | *int16 | *uint16 | *int32 | *uint32 | *int64 | *uint64 | *float32 | *float64 | *string | *bool |
+		chan int | chan uint | chan int8 | chan uint8 | chan int16 | chan uint16 | chan int32 | chan uint32 | chan int64 | chan uint64 | chan float32 | chan float64 | chan string | chan bool |
+		chan *int | chan *uint | chan *int8 | chan *uint8 | chan *int16 | chan *uint16 | chan *int32 | chan *uint32 | chan *int64 | chan *uint64 | chan *float32 | chan *float64 | chan *string | chan *bool
+}
+
+// SliceRemoveDuplicate slice去重
+func SliceRemoveDuplicate[T SliceType](arr []T) []T {
+	result := make([]T, 0, len(arr))
+	temp := make(map[T]struct{})
+	for _, item := range arr {
+		if _, ok := temp[item]; !ok {
+			temp[item] = struct{}{}
+			result = append(result, item)
+		}
+	}
+	return result
+}
+
+// SliceRemoveSpecificParam 从slice中移除特定的参数
+func SliceRemoveSpecificParam[T SliceType](arr []T, specific T) []T {
+	temp := make([]T, 0, len(arr))
+	for _, value := range arr {
+		if value != specific {
+			temp = append(temp, value)
+		}
+	}
+	return temp
+}
+
+// SliceIntersection slice的交集
+func SliceIntersection[T SliceType](first []T, last []T) []T {
+	result := make([]T, 0)
+	temp := make(map[T]struct{})
+	for _, value := range first {
+		if _, ok := temp[value]; !ok {
+			temp[value] = struct{}{}
+		}
+	}
+	for _, val := range last {
+		if _, ok := temp[val]; ok {
+			result = append(result, val)
+		}
+	}
+	return result
+}
+
+// SliceComplement slice的补集(subset必须是complete的子集)
+func SliceComplement[T SliceType](complete []T, subset []T) []T {
+	result := make([]T, 0)
+	temp := make(map[T]struct{})
+	for _, value := range subset {
+		if _, ok := temp[value]; !ok {
+			temp[value] = struct{}{}
+		}
+	}
+	for _, val := range complete {
+		if _, ok := temp[val]; !ok {
+			temp[val] = struct{}{} // 去重
+			result = append(result, val)
+		}
+	}
+	return result
+}
+
+// SlicePaginate slice 分页(1开始)
+func SlicePaginate[T SliceType](x []T, page int, size int) []T {
+	skip := (page - 1) * size
+	if skip > len(x) {
+		skip = len(x)
+	}
+	end := skip + size
+	if end > len(x) {
+		end = len(x)
+	}
+	return x[skip:end]
+}
+
+// SliceIsExist 指定参数值是否存在data[slice]中 false:不存在
+func SliceIsExist[T SliceType](data []T, param T) bool {
+	temp := make(map[T]struct{})
+	for _, value := range data {
+		if _, ok := temp[value]; !ok {
+			temp[value] = struct{}{}
+		}
+	}
+	if _, ok := temp[param]; !ok {
+		return false
+	}
+	return true
+}
